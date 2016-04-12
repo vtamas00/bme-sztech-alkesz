7f0a844362b815b1ffad98defa546efc1a6b6628
Varjasi Tamás <varjasitamas@gmail.com>
2016-04-12 09:05:44 +0200

    GUI Initial commit

---
+++ src/gui/Gui.java
@@ -0,0 +1,18 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class Gui extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	Gui() {
+		super("Alkesz");
+		setSize(400, 550);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(null);
+		
+		setVisible(true);
+	}
+
+}
